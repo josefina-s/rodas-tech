a16552e9b805c9c2f492287a2b531f71f8b5a5d6
mheredia <m.heredia92@gmail.com>
2023-04-30 22:56:30 -0300

    Navigation-Cloth CRUD

---
+++ app/src/main/java/com/example/rodastech/fragments/DetailProviderFragment.kt
@@ -0,0 +1,32 @@
+package com.example.rodastech.fragments
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.rodastech.R
+
+class DetailProviderFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = DetailProviderFragment()
+    }
+
+    private lateinit var viewModel: DetailProviderViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_detail_provider, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(DetailProviderViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
--- app/src/main/java/com/example/rodastech/fragments/ListClothFragment.kt
+++ app/src/main/java/com/example/rodastech/fragments/ListClothFragment.kt
@@ -1,6 +1,5 @@
 package com.example.rodastech.fragments
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -11,6 +10,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.rodastech.R
 import com.example.rodastech.adapters.ClothAdapter
+import com.example.rodastech.entities.Cloth
 import com.example.rodastech.entities.repositories.ClothRepository
 
 class ListClothFragment : Fragment() {
@@ -19,6 +19,7 @@ class ListClothFragment : Fragment() {
     lateinit var recyclerCloth : RecyclerView
     var clothRepository : ClothRepository = ClothRepository()
     lateinit var adapter : ClothAdapter
+
 
 
 
@@ -34,15 +35,27 @@ class ListClothFragment : Fragment() {
     override fun onStart() {
         super.onStart()
         recyclerCloth.layoutManager = LinearLayoutManager(context)
+//      PARA PROBAR LOS MÉTODOS DEL CRUD DESCOMENTAR SEGÚN EL MÉTODO QUE SE NECESITE mheredia 20230430
+//        adapter = ClothAdapter(clothRepository.cloths){position->
+////            val action = ListClothFragmentDirections.actionListClothFragmentToDetailClothFragment(clothRepository.cloths[position])
+////            val actionEdit = ListClothFragmentDirections.actionListClothFragmentToEditClothFragment(clothRepository.cloths[position])
+//            findNavController().navigate(actionDelete)
+//        }
 
         adapter = ClothAdapter(clothRepository.cloths){position->
-            val action = ListClothFragmentDirections.actionListClothFragmentToDetailClothFragment(clothRepository.cloths[position])
-            findNavController().navigate(action)
+            val actionDelete = ListClothFragmentDirections.actionListClothFragmentToDeleteClothFragment(clothRepository.cloths[position],clothRepository.cloths.toTypedArray())
+            findNavController().navigate(actionDelete)
         }
+
+
+//        adapter = ClothAdapter(clothRepository.cloths){
+//            val actionCreate= ListClothFragmentDirections.actionListClothFragmentToCreateClothFragment4(clothRepository.cloths.toTypedArray())
+//            findNavController().navigate(actionCreate)
+//            adapter.notifyDataSetChanged()
+//        }
         recyclerCloth.adapter = adapter
 
-
-
     }
+
 
 }
